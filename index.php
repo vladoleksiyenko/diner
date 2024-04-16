7ad2c53013cd1d2fee9ba13121bb1c5c4f12501c
Vlad Oleksiyenko <vladolek2003@gmail.com>
2024-04-16 13:50:01 -0400

    Added Session info for order1 page

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 require_once ('vendor/autoload.php');
 
 // Instantiate the F3 Base class
-$f3 = Base::instance();
+ $f3 = Base::instance();
 
 // Define a default route
 // https://voleksiyenko.greenriverdev.com/328/diner/
@@ -24,7 +24,6 @@ $f3->route('GET /', function() {
 });
 
 $f3->route('GET /menu/breakfast', function() {
-//    echo '<h1>My Breakfast Menu</h1>';
 
     // Render a view page
     $view = new Template();
@@ -32,11 +31,34 @@ $f3->route('GET /menu/breakfast', function() {
 });
 
 $f3->route('GET /menu/lunch', function() {
-//    echo '<h1>My Breakfast Menu</h1>';
 
     // Render a view page
     $view = new Template();
     echo $view->render('views/lunch-menu.html');
+});
+
+$f3->route('GET|POST /order1', function($f3) {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        // Get the data from the post array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        if (true) {
+            $f3 -> set('SESSION.food', $food);
+            $f3 -> set('SESSION.meal', $meal);
+        }
+    }
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+$f3->route('GET|POST /order2', function() {
+
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
 });
 
 
